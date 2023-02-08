09695e68a1c40447be7b57c110ba13b019e270f1
Ray <raymond_squared@live.com>
2023-02-08 11:46:46 +1100

    feat: put items

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,7 @@
 import {
-  DynamoDBClient, DynamoDBClientConfig, DescribeTableCommand, DescribeTableCommandInput,
+  DynamoDBClient, DynamoDBClientConfig,
+  DescribeTableCommand, DescribeTableCommandInput,
+  PutItemCommand, PutItemCommandInput,
 } from '@aws-sdk/client-dynamodb';
 
 // a client can be shared by different commands.
@@ -22,5 +24,49 @@ const run = async () => {
   }
 };
 
-// Uncomment this line to run execution within this file.
+interface Music {
+  artist: string;
+  songTitle: string;
+  albumTitle: string;
+  awards: number;
+}
+
+const putItem = async (music: Music) => {
+  await dynamoDBClient.send(
+    new PutItemCommand(
+      {
+        TableName: 'ray-music',
+        Item: {
+          artist: { S: music.artist },
+          songTitle: { S: music.songTitle },
+          albumTitle: { S: music.albumTitle },
+          awards: { N: music.awards.toString() },
+        },
+      } as PutItemCommandInput,
+    ),
+  );
+};
+
+const init = async () => {
+  try {
+    await putItem({
+      artist: 'No One You Know', songTitle: 'Call Me Today', albumTitle: 'Somewhat Famous', awards: 1,
+    } as Music);
+    await putItem({
+      artist: 'No One You Know', songTitle: 'Howdy', albumTitle: 'Somewhat Famous', awards: 2,
+    } as Music);
+    await putItem({
+      artist: 'Acme Band', songTitle: 'Happy Day', albumTitle: 'Songs About Life', awards: 10,
+    } as Music);
+    await putItem({
+      artist: 'Acme Band', songTitle: 'PartiQL Rocks', albumTitle: 'Another Album Title', awards: 8,
+    } as Music);
+
+    return;
+  } catch (err) {
+    console.log('Error', err);
+  }
+};
+
 run();
+init();
